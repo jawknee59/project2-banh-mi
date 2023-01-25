fc0f88e4b3c1981ad11a30df57c64cd0a9dc569b
jawkneewin <johnnytnguyen93@gmail.com>
2023-01-24 19:24:10 -0600

    User able to delete reviews

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -13,7 +13,7 @@ const router = express.Router()
 //// Routes                      ////
 /////////////////////////////////////
 
-// POST -> `/reviews/<someFruitId>`
+// POST -> `/reviews/<someBanhmiId>`
 // only loggedin users can post reviews
 // bc we have to refer to a banhmi, we'll do that in the simplest way via the route
 router.post('/:banhmiId', (req, res) => {
@@ -35,7 +35,7 @@ router.post('/:banhmiId', (req, res) => {
                 // save the banhmi
                 return banhmi.save()
             })
-            // respond with a 201 and the fruit itself
+            // respond with a 201 and the banhmi itself
             .then(banhmi => {
                 res.status(201).json({ banhmi: banhmi })
             })
@@ -47,6 +47,40 @@ router.post('/:banhmiId', (req, res) => {
     } else {
         res.sendStatus(401) //send a 401-unauthorized
     }
+})
+
+// DELETE -> `/reviews/delete/<someBanhmiId>/<someReviewId>`
+// make sure only the author of the review can delete the review
+router.delete('/delete/:banhmiId/:reviewId', (req, res) => {
+    // isolate the ids and save to variables so we don't have to keep typing req.params
+    const { banhmiId, reviewId } = req.params
+    // get the banhmi
+    Banhmi.findById(banhmiId)
+        .then(banhmi => {
+            // get the review, we'll use the built in subdoc method called .id()
+            const theReview = banhmi.reviews.id(reviewId)
+            console.log('this is the review to be deleted: \n', theReview)
+            // then we want to make sure the user is loggedIn, and that they are the author of the review
+            if (req.session.loggedIn) {
+                // if they are the author, allow them to delete
+                if (theReview.author == req.session.userId) {
+                    // we can use another built in method - remove()
+                    theReview.remove()
+                    banhmi.save()
+                    res.sendStatus(204) //send 204 no content
+                } else {
+                    // otherwise send a 401 - unauthorized status
+                    res.sendStatus(401)
+                }
+            } else {
+                // otherwise send a 401 - unauthorized status
+                res.sendStatus(401)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
 })
 
 //////////////////////////////
